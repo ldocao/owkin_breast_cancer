ca452309fca9030a0672d7866366713847b7fc7e
ldocao <ldocao@gmail.com>
2019-03-06 13:01:46 +0100

    refactor: propagate refactoring
    issue #3

--- predict_random.py
+++ predict_random.py
@@ -3,12 +3,12 @@
 import numpy as np
 import pandas as pd
 
-from test_patients import TestPatients
+from camelyon16 import TestPatients
 from challenge import Challenge
 
 
 TARGET = Challenge.TARGET_COLUMN
-patient_ids = TestPatients.ids()
+patient_ids = TestPatients().ids()
 n_patients = len(patient_ids)
 
 random_probabilities = np.random.uniform(size=n_patients)
