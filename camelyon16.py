f366eec17e717e99cc41d783b59542d1e00fde6b
ldocao <ldocao@gmail.com>
2019-03-07 10:48:33 +0100

    feat: load annotation
    split information in separate columns
    
    issue #6

--- camelyon16.py
+++ camelyon16.py
@@ -15,15 +15,17 @@ from challenge import Challenge
 
 class Camelyon16(object):
     ROOT_PATH = "/Users/ldocao/Documents/Personnel/Recherche emploi/data science/2019 02 05 Owkin/interview 3/data"
+    PREFIX = "ID_"
 
 
+    
     def resnet_features(self, patient_id):
         """Returns resnet features (without location)
 
         Parameters
         ----------
-        patient_id: str
-            patient identifier padded with Challenge.N_DIGITS leading zeros
+        patient_id: str or int
+            patient identifier
 
         """
         EXTENSION = ".npy"
@@ -63,17 +65,17 @@ class Camelyon16(object):
         absolute_paths = path.glob("*.npy")
         return absolute_paths
 
-
-    def _id_to_filename(self, patient_id):
+    
+    @classmethod
+    def _id_to_filename(cls, patient_id):
         """Returns prefixed name of file from patient_id
 
         Parameters
         ----------
-        patient_id: str
-            patient identifier padded with Challenge.N_DIGITS leading zeros
+        patient_id: str or int
+            patient identifier 
         """
-        PREFIX = "ID_"
-        return PREFIX + patient_id
+        return cls.PREFIX + str(patient_id).zfill(Challenge.N_DIGITS)
 
 
 
@@ -97,6 +99,7 @@ class TestPatients(Camelyon16):
         
 class TrainingPatients(Camelyon16):
     GROUND_TRUTH = "training_output_bis_EyawEvU.csv"
+    ANNOTATION = "train_tile_annotations.csv"
     
     def __init__(self, path="train_input"):
         """
@@ -107,13 +110,35 @@ class TrainingPatients(Camelyon16):
         """
         self.path = os.path.join(self.ROOT_PATH, path)
 
-
+    @property
     def ground_truths(self):
         """Returns ground truth labels as dataframe"""
         ground_truths = pd.read_csv(self.ground_truth_path())
         ground_truths.set_index(Challenge.INDEX_NAME, inplace=True)
         return ground_truths
-        
+
+    @property
+    def annotations(self):
+        """Returns splitted information on annotated tiles"""
+        cls = self.__class__
+        path = Path(self.path) / cls.ANNOTATION
+        FILENAME = "filename"
+        columns = [FILENAME, Challenge.TARGET_COLUMN]
+        annotations = pd.read_csv(path, names=columns, skiprows=1)
+        separate_info = lambda row: pd.Series(row.split("_"))
+        tile_infos = annotations[FILENAME].apply(separate_info)
+        tile_infos[7] = tile_infos[7].apply(lambda row: row.split(".")[0])
+        tile_infos.drop([0, 2, 3], axis=1, inplace=True)
+        tile_infos.rename(columns={1: Challenge.INDEX_NAME,
+                                   4: "tile_id",
+                                   5: "zoom_level",
+                                   6: "x",
+                                   7: "y"}, inplace=True)
+        tile_infos.index = annotations[FILENAME]
+        int_columns = ["zoom_level", "x", "y"]
+        tile_infos[int_columns] = tile_infos[["zoom_level", "x", "y"]].astype(int)
+        return tile_infos
+
 
     @classmethod
     def ground_truth_path(cls):
