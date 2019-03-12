6150f3a4b74ee46a0a3d4e736fdc15ceaefe757d
ldocao <ldocao@gmail.com>
2019-03-12 15:17:41 +0000

    feat: transfer learning on ResNet50
    run 1 in tensorboard. architecture = concatenation of flatten, max and avg pooling
    
    issue #15

---
+++ resnet50.py
@@ -0,0 +1,39 @@
+from keras.layers import Dense, Input, Dropout, Concatenate, GlobalMaxPooling2D, GlobalAveragePooling2D, Flatten
+from keras.applications.resnet50 import ResNet50
+from keras.optimizers import Adam
+from keras.losses import binary_crossentropy
+from keras.models import Model
+
+
+from image_classifier import ImageClassifier
+
+
+
+class ResNet:
+    INPUT_SHAPE = (96, 96, 3)
+
+    def __init__(self):
+        self.base_model = ResNet50(include_top=False,
+                                   input_shape=self.INPUT_SHAPE,
+                                   weights="imagenet",
+                                   pooling=None)
+    
+    def transfer_learning(self):
+        x = self.base_model.output
+        out1 = GlobalMaxPooling2D()(x)
+        out2 = GlobalAveragePooling2D()(x)
+        out3 = Flatten()(x)
+        out = Concatenate(axis=-1)([out1, out2, out3])
+        out = Dropout(0.5)(out)
+        out = Dense(1, activation="sigmoid", name="3_")(out)
+        model = Model(self.base_model.input, out)
+
+        for layer in self.base_model.layers:
+            layer.trainable = False
+
+        LEARNING_RATE = 0.00001 
+        model.compile(optimizer=Adam(LEARNING_RATE, decay=1e-6),
+                      loss=binary_crossentropy,
+                      metrics=['acc'])
+
+        return model
---
+++ transfer_resnet.py
@@ -0,0 +1,98 @@
+import ipdb
+
+import os
+import cv2
+import json
+
+import numpy as np
+import pandas as pd
+
+import keras
+from keras.callbacks import ModelCheckpoint
+from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+
+from challenge import Challenge
+from camelyon16 import TrainingPatients, AnnotatedTile
+from resnet50 import ResNet
+
+
+#hardware specs
+BATCH_SIZE = 32
+ANNOTATED_TILES_PATH = "/home/ldocao/owkin/data/train_input/annotated_tiles"
+
+#model
+resnet = ResNet().transfer_learning()
+print(resnet.summary())
+
+# raw data
+print("load data")
+training, validation = TrainingPatients().split_tiles_train_validation()
+tiles = TrainingPatients().annotations
+
+
+
+# # data augmentation
+print("define data augmentation")
+
+train_df = tiles.loc[training]
+train_datagen = ImageDataGenerator(shear_range=0.2,
+                                   zoom_range=0.2,
+#                                   brightness_range=0.1,
+                                   height_shift_range=0.2,
+                                   width_shift_range=0.2,
+                                   rotation_range=90,
+                                   horizontal_flip=True,
+                                   vertical_flip=True,
+                                   preprocessing_function=preprocess_input)
+train_generator = train_datagen.flow_from_dataframe(train_df.reset_index(),
+                                                    directory=ANNOTATED_TILES_PATH,
+                                                    x_col=tiles.index.name, y_col=Challenge.TARGET_COLUMN,
+                                                    target_size=ResNet.INPUT_SHAPE[:2],
+                                                    batch_size=BATCH_SIZE,
+                                                    class_mode='binary')
+
+validation_df = tiles.loc[validation]
+validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+validation_generator = validation_datagen.flow_from_dataframe(validation_df.reset_index(),
+                                                              directory=ANNOTATED_TILES_PATH,
+                                                              x_col=tiles.index.name, y_col=Challenge.TARGET_COLUMN,
+                                                              target_size=ResNet.INPUT_SHAPE[:2],
+                                                              batch_size=BATCH_SIZE,
+                                                              class_mode='binary')
+
+
+
+# #checkpoint strategy
+# print("define checkpoint strategy")
+# filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+# checkpoint = ModelCheckpoint(filepath,
+#                                                           monitor='val_acc',
+#                                                           verbose=1,
+#                                                           save_best_only=True,
+#                                                           mode='max')
+# callbacks_list = [checkpoint]
+
+n = 1 #run number
+tbCallBack = keras.callbacks.TensorBoard(log_dir=f'../tensorboard/run_{n}',
+                                         histogram_freq=0,
+                                         write_graph=True,
+                                         write_images=True)
+callbacks_list = [tbCallBack]
+
+# train the model
+print("train")
+N_STEPS_PER_EPOCH = len(training) // BATCH_SIZE
+VALIDATION_STEPS = len(validation) // BATCH_SIZE
+N_EPOCHS = 10
+history = resnet.fit_generator(train_generator,
+                               steps_per_epoch=N_STEPS_PER_EPOCH,
+                               epochs=N_EPOCHS,
+                               validation_data=validation_generator,
+                               validation_steps=VALIDATION_STEPS,
+                               use_multiprocessing=True,
+                               callbacks=callbacks_list)
+
+with open('transfer_nasnet_mobile.json', 'w') as fp:
+        json.dump(history.history, fp)
+
