9bce6a1a5d667b745d8bc9452daa7f154df63f7e
ldocao <ldocao@gmail.com>
2019-03-15 17:48:47 +0100

    feat: define XGBoost from sklearn syntax
    issue #17

--- tile_predictor.py
+++ tile_predictor.py
@@ -1,6 +1,7 @@
 import abc
+import numpy as np
 
-import xgboost as xgb
+from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
@@ -24,13 +25,15 @@ class TilePredictor:
 
     
 class LogisticRegressionL2(TilePredictor):
-    def train(self, x, y):
+
+    def __init__(self):
         PARAMS = {"penalty": "l2",
                   "C": 1.,
                   "solver": "liblinear"}
-        estimator = LogisticRegression(**PARAMS)
-        estimator.fit(x, y)
-        self.estimator = estimator #must possess predict_proba method
+        self.estimator = LogisticRegression(**PARAMS)
+
+    def train(self, x, y):
+        self.estimator.fit(x, y)
         return estimator
 
     def predict(self, x):
@@ -50,15 +53,28 @@ class LogisticRegressionL2(TilePredictor):
 
 
 class XGBoost(TilePredictor):
+    def __init__(self):
+        PARAMS = {"n_jobs": -1,
+                  "objective": "binary:logistic",
+                  "max_depth": 6,
+                  "learning_rate": 0.1}
+        self.estimator = XGBClassifier(**PARAMS)
+
+    def train(self, x, y):
+        self.estimator.fit(x, y)
+        return self.estimator
+
+    def grid_search(self, x, y):
+        GRID = {"max_depth": [int(f) for f in np.logspace(0, 2, 3)],
+                "learning_rate": np.logspace(-3, -1, 3),
+                "n_estimators": [int(f) for f in np.logspace(1, 2.5, 3)]}
+        grid_search = GridSearchCV(self.estimator, GRID, cv=self.__class__.N_FOLD, n_jobs=-1)
+        grid_search.fit(x, y)
+        return grid_search.best_params_
+        
     def cross_validation(self, x, y):
-        cls = self.__class__
-        PARAM = {'max_depth': 5,
-                 'eta': 0.5,
-                 'silent': 1,
-                 'objective': 'binary:logistic'}
-        auc = xgb.cv(PARAM,
-                     xgb.DMatrix(x, label=y),
-                     metrics="auc",
-                     nfold=cls.N_FOLD, stratified=True)
+        clf.fit(train_cluster_x,train_cluster_y)
+        clf.best_score_, clf.best_params_
+
         return auc
 
