347d90d590e86f157f65c36671c9188d7af4d50b
ldocao <ldocao@gmail.com>
2019-03-15 17:49:12 +0100

    feat: predict tile with xgb
    AUC 0.952, but much slower to compute
    
    issue #17

--- predict_tile.py
+++ predict_tile.py
@@ -6,19 +6,31 @@ import os
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+
 from camelyon16 import TrainingPatients
-from tile_predictor import LogisticRegressionL2
+from tile_predictor import LogisticRegressionL2, XGBoost
 
 
 #training
 features = TrainingPatients().stack_annotated_tile_features()
 ground_truths = TrainingPatients().annotations["Target"].values
-lr = LogisticRegressionL2()
-estimator = lr.train(features, ground_truths)
 
 
 #cross validation
-auc = lr.cross_validation(features, ground_truths)
-print("auc", auc)
-print("auc mean/std", auc.mean(), auc.std())
+train_index, test_index = train_test_split(range(len(features)),
+                                           train_size=0.8,
+                                           stratify=ground_truths)
+train_features = features[train_index, :]
+train_gt = ground_truths[train_index]
+
+validation_features = features[test_index, :]
+validation_gt = ground_truths[test_index]
+
+xgb = XGBoost().train(train_features, train_gt)
+validation_proba = xgb.predict_proba(validation_features)
+roc_auc_score(validation_gt, validation_proba[:, 1])
+print(roc_auc_score)
+
 
