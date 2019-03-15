f25bfb3478f8ed058b02c7afdac568f8ac22804b
ldocao <ldocao@gmail.com>
2019-03-15 19:10:54 +0100

    feat: add cross_val for lr
    issue #17

--- tile_predictor.py
+++ tile_predictor.py
@@ -2,44 +2,17 @@ import abc
 import numpy as np
 
 from xgboost import XGBClassifier
+import xgboost as xgb
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
 
 
-
+from camelyon16 import TrainingPatients
 
 class TilePredictor:
     N_FOLD = 5
     
-    @abc.abstractmethod
-    def train(self, x, y):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def predict(self, x):
-        raise NotImplementedError()
-
-
-
-
-    
-class LogisticRegressionL2(TilePredictor):
-
-    def __init__(self):
-        PARAMS = {"penalty": "l2",
-                  "C": 1.,
-                  "solver": "liblinear"}
-        self.estimator = LogisticRegression(**PARAMS)
-
-    def train(self, x, y):
-        self.estimator.fit(x, y)
-        return estimator
-
-    def predict(self, x):
-        return self.estimator.predict_proba(x)
-        
-
     def cross_validation(self, x, y, seed=0):
         cls = self.__class__
         cv = StratifiedKFold(n_splits=cls.N_FOLD,
@@ -52,29 +25,91 @@ class LogisticRegressionL2(TilePredictor):
 
 
 
-class XGBoost(TilePredictor):
+    def predict_tiles_of(self, ids):
+        """Returns positive probability for every tile of patient ids
+
+        Parameters
+        ----------
+        ids: iterable
+            list of patient ids
+        
+        Returns
+        -------
+        results: dict
+            prediction positive probabilities for each patient
+        """
+        results = {}
+        n = len(ids)
+        count = 0
+        for p in ids:
+            print(count/n)
+            resnet = TrainingPatients().resnet_features(p)
+            n_tiles = resnet.shape[0]
+            proba = self.estimator.predict_proba(resnet)[:,1]
+            results[p] = proba
+            count += 1
+        return results
+
+    @abc.abstractmethod
+    def train(self, x, y):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def predict(self, x):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def grid_search(self, x, y):
+        raise NotImplementedError
+
+
+
+    
+class LogisticRegressionL2(TilePredictor):
+
     def __init__(self):
-        PARAMS = {"n_jobs": -1,
-                  "objective": "binary:logistic",
-                  "max_depth": 6,
-                  "learning_rate": 0.1}
-        self.estimator = XGBClassifier(**PARAMS)
+        PARAMS = {"penalty": "l2",
+                  "C": 0.000774263682681127,
+                  "solver": "liblinear"}
+        self.estimator = LogisticRegression(**PARAMS)
 
     def train(self, x, y):
         self.estimator.fit(x, y)
         return self.estimator
 
-    def grid_search(self, x, y):
-        GRID = {"max_depth": [int(f) for f in np.logspace(0, 2, 3)],
-                "learning_rate": np.logspace(-3, -1, 3),
-                "n_estimators": [int(f) for f in np.logspace(1, 2.5, 3)]}
-        grid_search = GridSearchCV(self.estimator, GRID, cv=self.__class__.N_FOLD, n_jobs=-1)
-        grid_search.fit(x, y)
-        return grid_search.best_params_
+    def predict(self, x):
+        return self.estimator.predict_proba(x)
         
+    def grid_search(self, x, y):
+        cls = self.__class__
+        GRID = {"penalty": ["l2"],
+                "C": np.logspace(-4, -2, 10),
+                "solver": ["liblinear"]}
+        grid_search = GridSearchCV(self.estimator, GRID, n_jobs=-1, cv=cls.N_FOLD)
+        grid_search.fit(x, y)
+        print("best score", grid_search.best_score_)
+        return grid_search.best_params_
+
+
+
+class XGBoost(TilePredictor):
+    def __init__(self):
+        PARAMS = {'max_depth': 3,
+                  'eta': 0.3,
+                  'silent': 1,
+                  'objective': 'binary:logistic',
+                  'eval_metric': 'auc'}
+        self.params = PARAMS
+    
+    def train(self, x, y):
+        self.estimator.fit(x, y, metric='auc')
+        return self.estimator
+
     def cross_validation(self, x, y):
-        clf.fit(train_cluster_x,train_cluster_y)
-        clf.best_score_, clf.best_params_
-
-        return auc
-
+        cls = self.__class__
+        x_train = xgb.DMatrix(x, label=y)
+        results = xgb.cv(self.params, x_train,
+                         n_fold=cls.N_FOLD, num_boost_round=3,
+                         stratified=True, shuffle=True)
+        return results
+                
