46e07b39f9f3aabf76a8575ae84436acedb366e0
ldocao <ldocao@gmail.com>
2019-03-16 10:06:22 +0100

    feat: try SMOTE
    gains 2 points in AUC score
    
    issue #17

--- predict_tile.py
+++ predict_tile.py
@@ -6,7 +6,9 @@ import pickle
 
 import numpy as np
 import pandas as pd
+from collections import Counter
 
+from imblearn.over_sampling import SMOTE
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -20,10 +22,36 @@ training = TrainingPatients()
 features = training.stack_annotated_tile_features()
 ground_truths = training.annotations["Target"].values
 
-
-#train classifier
+#apply standard scaler anyway
 scaler = StandardScaler()
 features = scaler.fit_transform(features)
+
+
+#create validation set
+x_train, x_test, y_train, y_test = train_test_split(features, ground_truths,
+                                                    test_size=0.2,
+                                                    shuffle=True,
+                                                    stratify=ground_truths, random_state=1)
+
+clf = LogisticRegressionL2()
+clf.train(x_train, y_train)
+print(Counter(y_train))
+p_test = clf.predict(x_test)
+print("without smote AUC", roc_auc_score(y_test, p_test))
+
+
+#oversample positive labels
+smt = SMOTE()
+x_train, y_train = smt.fit_sample(x_train, y_train)
+print(Counter(y_train))
+clf = LogisticRegressionL2()
+clf.train(x_train, y_train)
+p_test = clf.predict(x_test)
+print("with smote AUC", roc_auc_score(y_test, p_test))
+
+ipdb.set_trace()
+
+#train classifier
 clf = LogisticRegressionL2()
 clf.train(features, ground_truths)
 
