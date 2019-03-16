9c71a65341b7508d0b8494ca747185f3df6f73c4
ldocao <ldocao@gmail.com>
2019-03-16 11:29:50 +0100

    feat: add cross validation check
    issue #17

--- predict_patient.py
+++ predict_patient.py
@@ -8,8 +8,10 @@ import pandas as pd
 
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, train_test_split
+
 
 from camelyon16 import TrainingPatients
 from patient_predictor import LogisticRegressionL2
@@ -34,11 +36,28 @@ for i in range(n_patients):
     tile_probas = training_predictions[p] #tile prediction values
     features_patients[i,:] = TileFeature(tile_probas).engineer()
 
+
+
+
+# run cross validation for check
+lr = LogisticRegressionL2()
+scaler = StandardScaler()
+pipeline = make_pipeline(scaler, lr.estimator)
+#pipeline = make_pipeline(lr.estimator)
+
+cv = StratifiedKFold(n_splits=5,
+                     shuffle=True,
+                     random_state=0)
+auc = cross_val_score(pipeline,
+                      X=features_patients, y=gt_patients,
+                      cv=cv, scoring="roc_auc", verbose=0)
+auc = np.array(auc)
+print(auc, auc.mean(), auc.std())
+
+
+#now train over the whole training set
 scaler = StandardScaler()
 features_patients = scaler.fit_transform(features_patients)
-
-# smt = SMOTE()
-# features_patients, gt_patients = smt.fit_sample(features_patients, gt_patients)
 lr = LogisticRegressionL2()
 lr.train(features_patients, gt_patients)
 
